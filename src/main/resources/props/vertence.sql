eecf044111e168846255d893c1c1b29a277c5538
dongye <dongye@hexindai.com>
2017-11-18 13:19:19 +0800

    commit

---
+++ src/main/resources/props/vertence.sql
@@ -0,0 +1,65 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.7.11 : Database - db_contract
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_contract` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `db_contract`;
+
+/*Table structure for table `attachment` */
+
+DROP TABLE IF EXISTS `attachment`;
+
+CREATE TABLE `attachment` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` int(1) DEFAULT NULL,
+  `foreignid` int(11) NOT NULL,
+  `filename` varchar(64) NOT NULL,
+  `savename` varchar(64) NOT NULL,
+  `createtime` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `detail` */
+
+DROP TABLE IF EXISTS `detail`;
+
+CREATE TABLE `detail` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `detailid` varchar(64) DEFAULT NULL,
+  `title` varchar(64) DEFAULT NULL,
+  `content` longtext,
+  `createtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_detailid` (`detailid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2230 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `news` */
+
+DROP TABLE IF EXISTS `news`;
+
+CREATE TABLE `news` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) DEFAULT NULL,
+  `content` longtext,
+  `status` tinyint(1) DEFAULT NULL COMMENT '0:删除 1:正常 2:置顶',
+  `createtime` datetime DEFAULT NULL,
+  `updatetime` datetime DEFAULT NULL,
+  `count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
